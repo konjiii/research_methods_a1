18f694cf6d6528144a81159c0c14d93c2c7a4a49
konjiii <kenji.opdam@gmail.com>
2024-04-26 15:32:55 +0200

    finished plots

--- a1.R
+++ a1.R
@@ -1,19 +1,58 @@
-max_sample <- 10000
+# first plot code
+# questionable practice
+# vector to store the p values
 pvalue_list <- c()
 
+# set seed
 set.seed(1)
-for (i in 10:max_sample) {
-  iq_nl <- rnorm(i, 101, 1)
-  iq_fi <- rnorm(i, 101, 1)
+
+# vector with the iq's
+iq_nl <- rnorm(1, 101, 15)
+iq_fi <- rnorm(1, 101, 15)
+
+# loop infinitely
+repeat {
+  # add a new test result every loop
+  iq_nl <- append(iq_nl, rnorm(1, 101, 15))
+  iq_fi <- append(iq_fi, rnorm(1, 101, 15))
   
+  # perform the t-test
   out <- t.test(iq_nl, iq_fi)
+  # append current p value to pvalue_list
   pvalue_list <- append(pvalue_list, out$p.value)
+  # if the p value is under 0.05 stop the test
   if (out$p.value < 0.05){
     break
   }
 }
 
-print(out)
-print(pvalue_list)
+# plot the p values
+plot(2:(length(pvalue_list)+1), pvalue_list, type = "l")
 
-plot(1:length(pvalue_list), pvalue_list)
+# second plot code
+# showing the first plot is wrong
+# vector to store the p values
+pvalue_list <- c()
+
+# set seed
+set.seed(1)
+
+# vector with the iq's
+iq_nl <- rnorm(1, 101, 15)
+iq_fi <- rnorm(1, 101, 15)
+
+# loop infinitely
+for (i in 1:6000) {
+  # add a new test result every loop
+  iq_nl <- append(iq_nl, rnorm(1, 101, 15))
+  iq_fi <- append(iq_fi, rnorm(1, 101, 15))
+  
+  # perform the t-test
+  out <- t.test(iq_nl, iq_fi)
+  # append current p value to pvalue_list
+  pvalue_list <- append(pvalue_list, out$p.value)
+}
+
+# plot the p values
+plot(2:(length(pvalue_list)+1), pvalue_list, type = "l")
+
