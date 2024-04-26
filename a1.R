f1820654875e5ae0d7b52298a1fe8c67fc6c5db3
Alex Snepvangers <apsnep@yahoo.com>
2024-04-26 18:06:27 +0200

    added sequential and rounding down Questionable practices

--- a1.R
+++ a1.R
@@ -7,25 +7,25 @@ pvalue_list <- c()
 set.seed(1)
 
 # vector with the heights
-iq_nl <- rnorm(1, 177, 10) #the std for nl/mne is 9.7cm rounded up to 10cm
-iq_mne <- rnorm(1, 177, 10) #the mean for nl is 177.1cm and for mne is 176.6 so rounded for both is 177cm
-
-# loop infinitely
-repeat {
-  # add a new test result every loop
-  iq_nl <- append(iq_nl, rnorm(1, 177, 10))
-  iq_mne <- append(iq_mne, rnorm(1, 177, 10))
+  height_nl <- rnorm(1, 177, 10) #the std for nl/mne is 9.7cm rounded up to 10cm
+  height_mne <- rnorm(1, 177, 10) #the mean for nl is 177.1cm and for mne is 176.6 so rounded for both is 177cm
   
-  # perform the t-test
-  out <- t.test(iq_nl, iq_mne)
+  # loop infinitely
+  repeat {
+    # add a new test result every loop
+    height_nl <- append(height_nl, rnorm(1, 177, 10))
+    height_mne <- append(height_mne, rnorm(1, 177, 10))
+    
+    # perform the t-test
+    out <- t.test(height_nl, height_mne)
   # append current p value to pvalue_list
   pvalue_list <- append(pvalue_list, out$p.value)
   # if the p value is under 0.05 stop the test
   if (out$p.value < 0.05){
     break
   }
-}
-
+  }
+  
 # plot the p values
 plot(2:(length(pvalue_list)+1), pvalue_list, type = "l")
 abline(h=0.05, col = "red", lty=2) #setting the p-value boundary
@@ -39,21 +39,52 @@ pvalue_list <- c()
 set.seed(1)
 
 # vector with the heights
-iq_nl <- rnorm(1, 177, 10)
-iq_fi <- rnorm(1, 177, 10)
+height_nl <- rnorm(1, 177, 10)
+height_mne <- rnorm(1, 177, 10)
 
 # loop infinitely
 for (i in 1:6000) {
   # add a new test result every loop
-  iq_nl <- append(iq_nl, rnorm(1, 177, 10))
-  iq_fi <- append(iq_mne, rnorm(1, 177, 10))
+  height_nl <- append(height_nl, rnorm(1, 177, 10))
+  height_mne <- append(height_mne, rnorm(1, 177, 10))
   
   # perform the t-test
-  out <- t.test(iq_nl, iq_mne)
+  out <- t.test(height_nl, height_mne)
   # append current p value to pvalue_list
   pvalue_list <- append(pvalue_list, out$p.value)
 }
 
 # plot the p values
 plot(2:(length(pvalue_list)+1), pvalue_list, type = "l")
+abline(h=0.05, col = "red", lty=2) #setting the p-value boundary
+
+#third plot code
+#sequential testing and rounding down p-values
+
+pvalue_list <- c()
+# vector with the heights
+height_nl <- rnorm(1, 177, 10) #the std for nl/mne is 9.7cm rounded up to 10cm
+height_mne <- rnorm(1, 177, 10) #the mean for nl is 177.1cm and for mne is 176.6 so rounded for both is 177cm
+
+# loop infinitely
+repeat {
+  # add a new test result every loop
+  height_nl <- append(height_nl, rnorm(1, 177, 10))
+  height_mne <- append(height_mne, rnorm(1, 177, 10))
+  
+  # perform the t-test
+  out <- t.test(height_nl, height_mne)
+  round_out <- floor(out$p.value * 10)/ 10 #rounds down the p-values of out
+  # append current p value to pvalue_list
+  pvalue_list <- append(pvalue_list,round_out)
+  # if the p value is under 0.05 stop the test
+  if (round_out < 0.05){
+    break
+  }
+}
+# plot the p values
+plot(2:(length(pvalue_list)+1), pvalue_list, type = "l")
+abline(h=0.05, col = "red", lty=2) #setting the p-value boundary
+
+
 
